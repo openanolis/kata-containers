daad4a8324f4b9e41c448d9570f4c5c049c700e0
qiliangfan <1035847702@qq.com>
2022-08-16 20:57:29 +0800

    add logger

--- src/dbs-cli/src/main.rs
+++ src/dbs-cli/src/main.rs
@@ -8,7 +8,14 @@ use parser::DBSArgs;
 use parser::run_with_cli;
 use anyhow::{anyhow, Context, Result};
 
+use log::{debug, error, log_enabled, info, Level};
+use env_logger;
+
+
+
 fn main() -> Result<()>{
+    env_logger::init();
+
     let args: DBSArgs = DBSArgs::parse();
 
     run_with_cli(&args)?;
